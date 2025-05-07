945cb592763c5896f3d04f6f52b75f028ab849e1
JulienMorin <j.morin88@outlook.fr>
2025-05-07 13:41:55 +0200

    Can now query citations from backend

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,4 +1,5 @@
 const services = require("../services/services");
+const { getAllCitationsWithAuthors } = require('../services/citationService');
 
 // This file is where you test for authorization when needed
 const test = async (req, res) => {
@@ -22,6 +23,34 @@ const test = async (req, res) => {
     }
 }
 
+const controller = {
+    getApi: (req, res) => {
+        console.log("req", req.user);
+        res.json({
+            message: "Hello from server!",
+            user: req.user
+        });
+    },
+    postApi: (req, res) => {
+        res.send(
+            `I received your POST request. This is what you sent me: ${req.body.post}`,
+        );
+    },
+
+    // New controller function for fetching citations
+    getCitationsApi: async (req, res) => {
+        try {
+            const citations = await getAllCitationsWithAuthors();
+
+            return res.status(200).send({ success: true, data: citations })
+        } catch (error) {
+            console.error("API Error fetching citations:", error);
+            res.status(500).json({ error: "Failed to fetch citations" });
+        }
+    }
+};
+
 module.exports = {
-    test
+    test,
+    ...controller
 }
