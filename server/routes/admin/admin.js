7a9983dd7a54e2fd1d97898970d6a91e8e2f7df9
Gaspachoo <gaspard.lefort@outlook.fr>
2025-05-15 22:13:15 +0530

    add admin routes

---
+++ server/routes/admin/admin.js
@@ -0,0 +1,12 @@
+// server/routes/admin/admin.js
+const express = require('express');
+const router = express.Router();
+const adminCtrl = require('../controllers/adminController');
+// (Optionally) add an `isAdmin` middleware here
+
+router.get('/fanfarons',       adminCtrl.listUsers);
+router.post('/fanfarons/:id/setPassword',  adminCtrl.setPassword);
+router.post('/fanfarons/:id/addAdminRole',    adminCtrl.addAdminRole);
+router.post('/fanfarons/:id/removeAdminRole', adminCtrl.removeAdminRole);
+
+module.exports = router;
