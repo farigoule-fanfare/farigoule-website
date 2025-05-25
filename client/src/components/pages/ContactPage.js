defaf7b0873f7b3ddaff8dd1f708e2c0e4841c7a
Gaspachoo <gaspard.lefort@outlook.fr>
2025-05-25 23:02:25 +0530

    add genre into portraits description

--- client/src/components/pages/ContactPage.js
+++ client/src/components/pages/ContactPage.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
     <ContentPageLayout title="Envoyer un message">
       <div className="container-contact-page">
         <p className="pTexteContact">
-          En bas de chaque page du site Web, vous trouverez le numéro de téléphone du président de la Farigoule, si vous voulez lui faire une déclaration d&apos;amour. Vous pouvez aussi nous envoyer un message directement sur Instagram ou sur notre page Facebook, en utilisant les boutons sur votre droite !
+          En bas de chaque page du site Web, vous trouverez le numéro de téléphone du/de la président(e) de la Farigoule, si vous voulez lui faire une déclaration d&apos;amour. Vous pouvez aussi nous envoyer un message directement sur Instagram ou sur notre page Facebook, en utilisant les boutons sur votre droite !
         </p>
       </div>
     </ContentPageLayout>
