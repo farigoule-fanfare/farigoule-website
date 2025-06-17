bd834c8353c41b72ace4057e25927e0247080112
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-17 20:38:54 +0530

    change mail form smtp for brevo

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -4,33 +4,33 @@ const router = express.Router();
 
 router.post('/contact', async (req, res) => {
   const { nom, email, message, destinataire } = req.body;
-
   if (!nom || !email || !message || !destinataire) {
     return res.status(400).json({ success: false, message: 'Champs manquants' });
   }
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email', //'smtp-relay.brevo.com',
+    const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
     port: 587,
-    secure: false,
+    secure: false, // True pour le port 465
     auth: {
-      user: 'erick.spencer@ethereal.email', //user: process.env.BREVO_SMTP_USER,
-      pass: 'aPAwzhm7FVkdARwtwz' // pass: process.env.BREVO_SMTP_PASS,
+        user: process.env.BREVO_SMTP_USER, // Ex: abcd1234@smtp-brevo.com
+        pass: process.env.BREVO_SMTP_PASS // Password d'une clé SMTP spécifique
     }
-  });
+    });
 
-  const mailOptions = {
-    from: `"Website" <erick.spencer@ethereal.email>`, // from: "Website" <${process.env.BREVO_SMTP_USER}>,
+    const mailOptions = {
+    from: `"Farigoule Website" <${process.env.CONTACT_MAIL_FROM}>`, // Doit être un email vérifié dans Brevo
     to: destinataire,
     replyTo: email,
     subject: 'Message via le site de la Farigoule',
     text: `Message de ${nom} (${email}) :\n\n${message}`,
-  };
+    };
 
   try {
     await transporter.sendMail(mailOptions);
     res.status(200).json({ success: true, message: 'Message envoyé' });
   } catch (error) {
+    console.error('[MAIL ERROR]', error);
     res.status(500).json({
       success: false,
       message: 'Erreur serveur lors de l’envoi',
