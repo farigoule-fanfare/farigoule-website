a8906d6d3158d7c59d5f805afa100cd661b86c90
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-25 16:06:22 +0530

    remove redundant get fanfarons routes

--- server/routes/admin/fanfarons.js
+++ server/routes/admin/fanfarons.js
@@ -27,7 +27,6 @@ const { protect, authorize } = require('../../middleware/authMiddleware');
 // ⚠️-protège toutes les routes ci-dessous
 router.use(protect, authorize(['admin']));
 
-router.get('/',        ctrl.getAllFanfarons);
 router.post('/',       upload.single('photoFanfaron'), ctrl.createFanfaron);
 router.put('/:id',     upload.single('photoFanfaron'), ctrl.updateFanfaron);
 router.delete('/:id',                    ctrl.removeFanfaron);
