4ed8fe5f00f3fb4c5a9957ddc19d1714e2ac15e8
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-25 16:08:06 +0530

    remove comments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,15 +3,13 @@ const controllers = require("../controllers/controllers");
 
 const router = express.Router();
 
-// New route for citations
-router.get("/citations", controllers.getCitationsApi);
-
 // Public routes
 router.get('/diapos', controllers.getAllDiaposApi);           // Route for all diapos
 router.get('/diapos/latest', controllers.getLatestDiaposApi);       // Route for latest diapos
 router.get('/diapos/random', controllers.getRandomDiapoApi); // Route for random diapo
 router.get('/contrats/upcoming', controllers.getUpcomingContratsApi); // Route for upcoming contrats
 router.get('/contrats/past', controllers.getPastContratsApi);       // Route for past contrats
+router.get("/citations", controllers.getCitationsApi); // Route for all citations
 router.get('/fanfarons', controllers.getAllFanfaronsApi); // Route for all fanfarons
 
 module.exports = router;
