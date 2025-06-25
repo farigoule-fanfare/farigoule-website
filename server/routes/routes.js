30a1efc544f10859d82b255c86d5dba23406183a
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-25 19:51:01 +0530

    move some get function to contratsController

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const controllers = require("../controllers/controllers");
 const citationControllers = require("../controllers/citationsController");
 const diaposControllers = require("../controllers/diaposController");
+const contratsControllers = require("../controllers/contratsController");
 
 const router = express.Router();
 
@@ -9,8 +10,8 @@ const router = express.Router();
 router.get('/diapos', diaposControllers.getAllDiaposApi);           // Route for all diapos
 router.get('/diapos/latest', diaposControllers.getLatestDiaposApi);       // Route for latest diapos
 router.get('/diapos/random', diaposControllers.getRandomDiapoApi); // Route for random diapo
-router.get('/contrats/upcoming', controllers.getUpcomingContratsApi); // Route for upcoming contrats
-router.get('/contrats/past', controllers.getPastContratsApi);       // Route for past contrats
+router.get('/contrats/upcoming', contratsControllers.getUpcomingContratsApi); // Route for upcoming contrats
+router.get('/contrats/past', contratsControllers.getPastContratsApi);       // Route for past contrats
 router.get("/citations", citationControllers.getCitationsApi); // Route for all citations
 router.get("/citations/ordered", citationControllers.getAllCitationsOrdered); // Route for all citations
 router.get('/fanfarons', controllers.getAllFanfaronsApi); // Route for all fanfarons
