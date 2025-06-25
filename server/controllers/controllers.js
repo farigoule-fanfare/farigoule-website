f57aae2b94920ecb9505d333212944d6088e80ad
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-25 18:11:55 +0530

    remove useless service

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,30 +1,7 @@
-const services = require("../services/services");
 const { getAllCitationsWithAuthors } = require('../services/citationService');
 const { getAllFanfarons } = require('../services/fanfaronService');
 const diapoService = require('../services/diapoService');
 const contratService = require('../services/contratService');
-
-// This file is where you test for authorization when needed
-const test = async (req, res) => {
-    let errorReason = null
-    try {
-
-        const d = await services.test()
-
-        if (!d?.success) {
-            errorReason = d?.errorReason
-            throw new Error(d?.error)
-        }
-
-        return res.status(200).send({ success: true, data: d?.data })
-    }
-    catch (e) {
-        if (["invalidToken"].includes(errorReason)) {
-            return res.status(400).send({ success: false, errorReason })
-        }
-        return res.status(500).send({ success: false, error: e, errorReason: errorReason })
-    }
-}
 
 const controller = {
     getApi: (req, res) => {
@@ -140,6 +117,5 @@ const controller = {
 };
 
 module.exports = {
-    test,
     ...controller
 }
