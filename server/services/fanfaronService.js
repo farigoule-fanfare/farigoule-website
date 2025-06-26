7e4e7c9a07503a023c8b5a23d0e9bd9b9b310a93
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-26 22:24:50 +0530

    move fanfarons image mapping to service

--- server/services/fanfaronService.js
+++ server/services/fanfaronService.js
@@ -1,20 +1,34 @@
+// services/fanfaronService.js
 const fanfaronRepo = require('../repositories/fanfaronRepository');
 
+const BASE_URL = process.env.REACT_APP_RESTAPI_SERVER_URI;
+const buildPhotoUrl = (photo) =>
+  `${BASE_URL}/public/uploads/fanfarons/${photo}`;
+
+const addPhotoUrl = (f) => ({
+  ...f,
+  photoUrl: buildPhotoUrl(f.photo),
+});
+
 const fanfaronService = {
-  getAllFanfarons() {
-    return fanfaronRepo.findAll();
+  async getAllFanfarons() {
+    const fanfarons = await fanfaronRepo.findAll();
+    return fanfarons.map(addPhotoUrl);
   },
 
-  getAllFanfaronsAnnuaire() {
-    return fanfaronRepo.findAllAnnuaire();
+  async getAllFanfaronsAnnuaire() {
+    const fanfarons = await fanfaronRepo.findAllAnnuaire();
+    return fanfarons.map(addPhotoUrl);
   },
 
-  createFanfarons(data) {
-    return fanfaronRepo.create(data);
+  async createFanfarons(data) {
+    const created = await fanfaronRepo.create(data);
+    return addPhotoUrl(created);
   },
 
-  updateFanfarons(id, data) {
-    return fanfaronRepo.update(id, data);
+  async updateFanfarons(id, data) {
+    const updated = await fanfaronRepo.update(id, data);
+    return addPhotoUrl(updated);
   },
 
   deleteFanfarons(id) {
