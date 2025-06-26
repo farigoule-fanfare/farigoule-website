23a35291c52d618cb4d000e5bcd5b252a8e73650
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-26 18:26:49 +0530

    remove api from handler names

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,10 +4,10 @@ const ctrl = require("../controllers/userController");
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 // Public routes
-router.get('/current-president', ctrl.getCurrentPresidentApi);
+router.get('/current-president', ctrl.getCurrentPresident);
 
 // Protected routes (authenticated users)
-router.put('/profile', protect, ctrl.updateProfileApi);
+router.put('/profile', protect, ctrl.updateProfile);
 
 // Private routes(admin only)
 router.post('/:id/addAdminRole', [protect, authorize(['admin'])], ctrl.addAdminRole);
