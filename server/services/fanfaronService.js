033160b4dcff022ef676e57d8bfcc063929e9105
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-26 14:03:46 +0530

    add repository for fanfaron service

--- server/services/fanfaronService.js
+++ server/services/fanfaronService.js
@@ -1,139 +1,25 @@
-const db = require('./databaseService');
+const fanfaronRepo = require('../repositories/fanfaronRepository');
 
+const fanfaronService = {
+  getAllFanfarons() {
+    return fanfaronRepo.findAll();
+  },
 
-module.exports = {
-  /**
-   * Récupère tous les fanfarons
-   * @returns {Promise<Array>} liste des fanfarons
-   */
-  getAllFanfarons: () =>
-    new Promise((resolve, reject) => {
-      db.all('SELECT id, surnom, instrument, promo, bureau, description, photo FROM fanfarons', (err, rows) => {
-        if (err) reject(err);
-        else resolve(rows);
-      });
-    }),
+  getAllFanfaronsAnnuaire() {
+    return fanfaronRepo.findAllAnnuaire();
+  },
 
-  /**
-   * Récupère tous les fanfarons avec toutes leurs infos pour la page admin et annuaire
-   * @returns {Promise<Array>} liste des fanfarons
-   */
-  getAllFanfaronsAnnuaire: () =>
-    new Promise((resolve, reject) => {
-      db.all('SELECT * FROM fanfarons', (err, rows) => {
-        if (err) reject(err);
-        else resolve(rows);
-      });
-    }),
-  
-  /**
-   * Crée un nouveau fanfaron
-   * @param {Object} data - champs du fanfaron
-   * @returns {Promise<Object>} fanfaron inséré (avec id)
-   */
-  createFanfarons: data =>
-    new Promise((resolve, reject) => {
-      const {
-        surnom,
-        nom,
-        prenom,
-        instrument,
-        promo,
-        bureau,
-        email,
-        tel,
-        description,
-        photo
-      } = data;
-      const stmt = db.prepare(
-        `INSERT INTO fanfarons
-         (surnom,nom,prenom, instrument, promo, bureau, email, tel, description, photo)
-         VALUES (?, ?,?,?, ?, ?, ?, ?, ?, ?)`
-      );
-      stmt.run(
-        surnom,
-        nom,
-        prenom,
-        instrument,
-        promo,
-        bureau,
-        email,
-        tel,
-        description,
-        photo,
-        function(err) {
-          if (err) return reject(err);
-          const newF = db.prepare('SELECT * FROM fanfarons WHERE id = ?').get(this.lastID);
-          resolve(newF);
-        }
-      );
-    }),
+  createFanfarons(data) {
+    return fanfaronRepo.create(data);
+  },
 
-  /**
-   * Met à jour un fanfaron existant
-   * @param {number|string} id - identifiant du fanfaron
-   * @param {Object} data - champs à mettre à jour
-   * @returns {Promise<Object>} fanfaron mis à jour
-   */
-  updateFanfarons: (id, data) =>
-    new Promise((resolve, reject) => {
-      const {
-        surnom,
-        nom,
-        prenom,
-        instrument,
-        promo,
-        bureau,
-        email,
-        tel,
-        description,
-        photo
-      } = data;
-      const stmt = db.prepare(
-        `UPDATE fanfarons
-         SET surnom = ?,
-             nom = ?,
-             prenom = ?,
-             instrument = ?,
-             promo = ?,
-             bureau = ?,
-             email = ?,
-             tel = ?,
-             description = ?,
-             photo = COALESCE(?, photo)
-         WHERE id = ?`
-      );
-      stmt.run(
-        surnom,
-        nom,
-        prenom,
-        instrument,
-        promo,
-        bureau,
-        email,
-        tel,
-        description,
-        photo,
-        id,
-        function(err) {
-          if (err) return reject(err);
-          const updated = db.prepare('SELECT * FROM fanfarons WHERE id = ?').get(id);
-          resolve(updated);
-        }
-      );
-    }),
+  updateFanfarons(id, data) {
+    return fanfaronRepo.update(id, data);
+  },
 
-  /**
-   * Supprime un fanfaron par son identifiant
-   * @param {number|string} id - identifiant du fanfaron
-   * @returns {Promise<void>}
-   */
-  deleteFanfarons: id =>
-    new Promise((resolve, reject) => {
-      const stmt = db.prepare('DELETE FROM fanfarons WHERE id = ?');
-      stmt.run(id, function(err) {
-        if (err) reject(err);
-        else resolve();
-      });
-    })
+  deleteFanfarons(id) {
+    return fanfaronRepo.remove(id);
+  },
 };
+
+module.exports = fanfaronService;
