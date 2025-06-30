06a0a154c2ce93c63eb95c1f5257d68468455722
Gaspachoo <gaspard.lefort@outlook.fr>
2025-06-30 22:54:54 +0530

    factor adminPanel pages

--- server/routes/diaposRoutes.js
+++ server/routes/diaposRoutes.js
@@ -27,14 +27,20 @@ const router = require('express').Router();
 const ctrl   = require('../controllers/diaposController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
-// Public : 5 diapos aléatoires
+// Public routes
 router.get('/', (req, res, next) => {
   if (req.query.order === undefined) req.query.order = 'random';
   if (req.query.limit === undefined) req.query.limit = '5';
   ctrl.listDiapos(req, res, next);});
 
 // Private routes
-router.get('/ordered', [protect, authorize(['admin'])], (req, res, next) => { req.query.order = 'desc'; ctrl.listDiapos(req, res, next); });
+router.get('/ordered',
+  [protect, authorize(['admin'])],
+  (req, res, next) => {
+    req.query.order = 'desc';
+    req.query.all   = 'true';
+    ctrl.listDiapos(req, res, next);
+  });
 router.post('/',       upload.single('file'), [protect, authorize(['admin'])], ctrl.addDiapo);
 router.put('/:id',     upload.single('file'), [protect, authorize(['admin'])], ctrl.updateDiapo);
 router.delete('/:id',                         [protect, authorize(['admin'])], ctrl.deleteDiapo);
