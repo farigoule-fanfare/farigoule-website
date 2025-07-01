5bae12307cbc93a41a018fc5cff883f138258229
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-01 23:18:12 +0530

    move Auth to feature

--- client/src/components/Profile/UserProfile.js
+++ client/src/components/Profile/UserProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '@features/auth';
 import { axiosWrapper } from '@services/axiosUtils';
 import ContentPageLayout from '../layout/ContentPageLayout';
 import { useNavigate } from 'react-router-dom';
