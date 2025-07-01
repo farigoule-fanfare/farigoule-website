142f36664ca0edb8effa13ca71e045743ab5e2b6
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-01 23:17:55 +0530

    remove useless files

--- client/src/services/postLogout.js
+++
@@ -1,9 +0,0 @@
-import { axiosWrapper } from '@services/axiosUtils'
-
-export const postLogout = async () => {
-    return axiosWrapper({
-        data: {},
-        method: "post",
-        url: "api/identity/logout",
-    })
-}
