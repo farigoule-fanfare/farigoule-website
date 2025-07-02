27cc5c9fa7550813d4715e6a03b867a9adcb1b0e
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-02 11:36:53 +0530

    move requireAuth to auth feature

--- client/src/features/auth/index.js
+++ client/src/features/auth/index.js
@@ -1,3 +1,4 @@
 export { default as LoginPage } from './pages/LoginPage';
 export * from './context/AuthContext';
 export { default as AuthContext } from './context/AuthContext';
+export { default as RequireAuth } from './components/RequireAuth';
