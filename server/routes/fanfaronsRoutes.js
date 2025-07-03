19ebeaaeacf8f8273c9f4af8868ef860e8c3ced2
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-03 23:37:04 +0530

    migrate annuaire feature to new client organization

--- server/routes/fanfaronsRoutes.js
+++ server/routes/fanfaronsRoutes.js
@@ -26,7 +26,9 @@ const { protect, authorize } = require('../middleware/authMiddleware');
 router.get('/', ctrl.listFanfarons); // Route for all fanfarons
 
 // Private routes
-router.get('/annuaire',                               [protect, authorize(['admin'])], ctrl.listFanfaronsAnnuaire);
+router.get('/annuaire',                               [protect], ctrl.listFanfaronsAnnuaire);
+
+// Admin routes
 router.post('/',      upload.single('photoFanfaron'), [protect, authorize(['admin'])], ctrl.createFanfaron);
 router.put('/:id',    upload.single('photoFanfaron'), [protect, authorize(['admin'])], ctrl.updateFanfaron);
 router.delete('/:id',                                 [protect, authorize(['admin'])], ctrl.removeFanfaron);
