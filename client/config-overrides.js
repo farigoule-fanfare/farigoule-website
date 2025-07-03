362d68a011d42ca2e1f34444f7f46cf84ec30cf4
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-03 10:35:46 +0530

    clean config-overrides

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -4,17 +4,10 @@ const path = require('path');
 module.exports = override(
   addWebpackAlias({
     // Add your webpack aliases here
-    "@":        path.resolve(__dirname, "src"),
-    "@assets":  path.resolve(__dirname, "src/assets"),
-    '@features': path.resolve(__dirname, 'src/features'),
-    '@shared':   path.resolve(__dirname, 'src/shared'),
-    '@shell':   path.resolve(__dirname, 'src/shell'),
-
-    //Temporary for migration
-    "@hooks":   path.resolve(__dirname, "src/hooks"),
-    "@services":path.resolve(__dirname, "src/services"),
-    "@components": path.resolve(__dirname, "src/components"),
-    '@context': path.resolve(__dirname, 'src/context'),
-    '@providers': path.resolve(__dirname, 'src/providers'),
+    "@":         path.resolve(__dirname, "src"),
+    "@assets":   path.resolve(__dirname, "src/assets"),
+    '@features': path.resolve(__dirname, "src/features"),
+    '@shell':    path.resolve(__dirname, "src/shell"),
+    "@services": path.resolve(__dirname, "src/services"),
   })
 );
